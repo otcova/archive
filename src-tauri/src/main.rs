92e54c6b87953603f54b3cbb5f95f9bc17d3424f
otcova <otgercomas@gmail.com>
2022-05-10 09:21:53 +0200

    sketch.svg and rewire tauri

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,16 +1,16 @@
-// #![cfg_attr(
-//     all(not(debug_assertions), target_os = "windows"),
-//     windows_subsystem = "windows"
-// )]
+#![cfg_attr(
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
+)]
 
 mod database;
 
 fn main() {
-    // start_client();
+    start_client();
 }
 
-// fn start_client() {
-//     tauri::Builder::default()
-//         .run(tauri::generate_context!())
-//         .expect("error while running tauri application");
-// }
+fn start_client() {
+    tauri::Builder::default()
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
+}
