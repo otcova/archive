88afa89f8936ab915d49492b8e153f4625797b9e
otcova <otgercomas@gmail.com>
2022-06-08 21:26:35 +0200

    database chunk open & create

--- src-tauri/src/chunked_database/chunk.rs
+++ src-tauri/src/chunked_database/chunk.rs
@@ -20,16 +20,24 @@ pub struct Chunk<T: Item> {
 
 impl<T: Item> Chunk<T> {
     /// Open on a empty direcory will create a blanck database
-    pub fn open(path: &PathBuf) -> Result<Self> {
-        match Database::open(path) {
-            Ok(database) => Ok(Self { database }),
+    pub fn open(path: &PathBuf) -> Result<Self>  {
+        match Self::create_chunk(path) {
             Err(err) => match *err {
-                ErrorKind::NotFound => Ok(Self {
-                    database: Database::create(path)?,
-                }),
+                ErrorKind::AlreadyExist => Self::open_chunk(path),
                 err => err.into(),
             },
+            database_chunk => database_chunk,
         }
+    }
+    fn open_chunk(path: &PathBuf) -> Result<Self> {
+        Ok(Self {
+            database: Database::open(path)?,
+        })
+    }
+    fn create_chunk(path: &PathBuf) -> Result<Self> {
+        Ok(Self {
+            database: Database::create(path)?,
+        })
     }
 
     pub fn min_sort(&self) -> BinaryHeap<Id> {
@@ -37,3 +45,23 @@ impl<T: Item> Chunk<T> {
     }
 }
 
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test_utils::*;
+
+    #[derive(Debug, Serialize, Deserialize, Default, Clone, Copy)]
+    struct Data(i32);
+    impl Item for Data {
+        fn date(&self) -> i32 {
+            self.0
+        }
+    }
+
+    #[test]
+    fn open_database_chunk_on_empty_dir() {
+        let tempdir = TempDir::new();
+        let chunk = Chunk::<Data>::open(&tempdir.path).unwrap();
+        assert_eq!(chunk.database.data.items.len(), 0);
+    }
+}
