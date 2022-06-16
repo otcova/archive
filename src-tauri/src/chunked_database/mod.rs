c62c9909d763f3588f7e879e0b112bf2c658d462
otcova <otgercomas@gmail.com>
2022-06-16 15:20:19 +0200

    chunked database push, len, read test

--- src-tauri/src/chunked_database/mod.rs
+++ src-tauri/src/chunked_database/mod.rs
@@ -22,13 +22,15 @@ pub enum Uid {
 pub struct ChunkedDatabase<T: Item> {
     dynamic: Chunk<T>,
     ancient: Chunk<T>,
+    max_dynamic_len: usize,
 }
 
 impl<T: Item> ChunkedDatabase<T> {
-    pub fn open(path: &PathBuf) -> Result<Self> {
+    pub fn open(path: &PathBuf, max_dynamic_len: usize) -> Result<Self> {
         Ok(Self {
             dynamic: Chunk::open(&path.join("dynamic"))?,
             ancient: Chunk::open(&path.join("ancient"))?,
+            max_dynamic_len,
         })
     }
 
@@ -57,19 +59,47 @@ impl<T: Item> ChunkedDatabase<T> {
             Uid::ANCIENT(id) => self.ancient.database.data.items.read(id),
         }
     }
+    pub fn len(&self) -> usize {
+        self.dynamic.database.data.items.len() + self.ancient.database.data.items.len()
+    }
 
-    // if dynamic database has more than max items,
-    // then the oldest items are moved to the ancient database
-    // pub fn transfer_old_items(&mut self, min: usize, max: usize) {
-    //     let dyn_len = self.dynamic.database.data.items.len();
-    //     if dyn_len > max {
-    //         let mut heap = self.dynamic.min_sort();
-    //         for _ in 0..dyn_len - min {
-    //             let id = heap.pop().unwrap();
-    //             let item = self.dynamic.database.data.items.read(id).clone().unwrap();
-    //             self.ancient.database.data.items.push(item);
-    //             self.dynamic.database.data.items.delete(id);
-    //         }
-    //     }
-    // }
+    /// Moves items from the dynamic chunk to the ancient chunk to satisfy 'max_dynamic_len'
+    fn move_old_items(&mut self) {}
+}
+
+impl<T: Item> Drop for ChunkedDatabase<T> {
+    fn drop(&mut self) {
+        self.move_old_items();
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+    use crate::test_utils::*;
+    use serde::*;
+
+    #[derive(Debug, Serialize, Deserialize, Default, Clone, Copy, PartialEq, Eq)]
+    struct Data(i32);
+    impl Item for Data {
+        fn date(&self) -> i32 {
+            self.0
+        }
+    }
+
+    #[test]
+    fn push_len_and_read() {
+        let tempdir = TempDir::new();
+        
+        let mut db = ChunkedDatabase::<Data>::open(&tempdir.path, 100).unwrap();
+        let id_54 = db.push(Data(54));
+        let id_13 = db.push(Data(13));
+        let id_223 = db.push(Data(223));
+        
+        assert_eq!(3, db.len());
+        
+        assert_eq!(Some(Data(13)), *db.read(id_13));
+        assert_eq!(Some(Data(223)), *db.read(id_223));
+        assert_eq!(Some(Data(54)), *db.read(id_54));
+    }
 }
