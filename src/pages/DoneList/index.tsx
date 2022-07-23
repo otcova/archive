46c8123db5c99a236919bbd3936e42cdb1be14ea
otcova <otgercomas@gmail.com>
2022-07-23 15:46:41 +0200

    page done list filter inputs

--- src/pages/DoneList/index.tsx
+++ src/pages/DoneList/index.tsx
@@ -1,8 +1,23 @@
 import OrderList, { lableByDate } from '../../templates/OrderList'
 import { createHook } from '../../database/expedientHook'
+import style from "./DoneList.module.sass"
+import InputText from '../../atoms/InputText'
+import { createEffect, createSignal } from 'solid-js'
 
 export default function DoneList() {
-	
+
+	const [inputVIN, setInputVin] = createSignal<string>("")
+	const [inputUser, setInputUser] = createSignal<string>("")
+	const [inputBody, setInputBody] = createSignal<string>("")
+
+	const [list, setList] = createSignal<[]>()
+
+	createEffect(() => {
+		const filter = inputVIN() + inputUser() + inputBody() != ""
+		if (!filter) return setList(orderList)
+		setList([])
+	})
+
 	const [orderList] = createHook("list_oreders", {
 		sort_by: "Newest",
 		from_date: 10000000,
@@ -12,6 +27,15 @@ export default function DoneList() {
 		show_pending: false,
 		show_urgent: false,
 	})
-	
-	return <OrderList orderList={() => [...lableByDate(orderList())]} />
+
+	return <>
+		<div class={style.input_row}>
+			<div class={style.input_vin}>
+				<InputText placeholder='Matricula / VIN' onChange={setInputVin} />
+			</div>
+			<InputText placeholder='Usuari' onChange={setInputUser} />
+			<InputText placeholder='Cos' onChange={setInputBody} />
+		</div>
+		<OrderList orderList={() => [...lableByDate(list())]} />
+	</>
 }
