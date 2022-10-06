c8cdfde4e40670b5599445993431f34e2e3485dd
otcova <otgercomas@gmail.com>
2022-10-06 10:19:03 +0200

    Save previous release

--- publish-release.js
+++ publish-release.js
@@ -10,31 +10,41 @@ const exitWithError = msg => {
 
 let raw_config_data
 try { raw_config_data = fs.readFileSync("./src-tauri/tauri.conf.json") }
-catch (error) { exitWithError("unable to read tauri.conf.json") }
+catch (error) { exitWithError("Unable to read tauri.conf.json") }
 
 let config
 try { config = JSON.parse(raw_config_data) }
-catch (error) { exitWithError("unable to parse tauri.conf.json") }
+catch (error) { exitWithError("Unable to parse tauri.conf.json") }
 
 if (!config.package?.version)
 	exitWithError("missing field 'package.version' on tauri.conf.json")
 let version = config.package?.version
 
-////////////  Moving artifacts  /////////////////////////////////////////////////
+//////////  Move artifacts  /////////////////////////////////////////////////
 
-try { fs.rmSync("./releases", { recursive: true, force: true }) }
-catch (error) { exitWithError("could not clean previous releases because: " + error) }
+let msi_dir
+try { msi_dir = fs.readdirSync(`./releases/msi`) }
+catch (error) { exitWithError("Could not read directory because: " + error) }
 
-try { fs.cpSync("./src-tauri/target/release/bundle", "./releases", { recursive: true }) }
-catch (error) { exitWithError("could not copy releases because: " + error) }
+try { fs.mkdirSync("./releases/past-msi", { recursive: true }) }
+catch (error) { exitWithError("Could not create path because: " + error) }
 
-////////////  Creating version_report.json  ///////////////////////////////////////////
+for (const file of msi_dir) {
+	let name = "archive" + (file.match(/(\.msi|\.msi\.zip|\.msi\.zip\.sig)$/g)[0] || "");
+	try { fs.renameSync("./releases/msi/" + file, "./releases/past-msi/" + name) }
+	catch (error) { exitWithError("Could not move file because: " + error) }
+}
+
+try { fs.cpSync("./src-tauri/target/release/bundle", "./releases", { recursive: true, force: true }) }
+catch (error) { exitWithError("Could not copy releases because: " + error) }
+
+////////////  Create version_report.json  ///////////////////////////////////////////
 
 let artifact_path = `/releases/msi/archive_${version}_x64_en-US.msi.zip`
 
 let signature
 try { signature = fs.readFileSync("." + artifact_path + ".sig").toString() }
-catch (error) { exitWithError("could not load signature because: " + error) }
+catch (error) { exitWithError("Could not load signature because: " + error) }
 
 const version_report = {
 	version,
@@ -47,7 +57,7 @@ const version_report = {
 	}
 }
 try { fs.writeFileSync("./releases/version_report.json", JSON.stringify(version_report, null, "\t")) }
-catch (error) { exitWithError("could not write version_report.json because: " + error) }
+catch (error) { exitWithError("Could not write version_report.json because: " + error) }
 
 ////////////  Increase Version  /////////////////////////////////////////////////
 
@@ -59,23 +69,23 @@ let new_version = major + "." + median + "." + (minor + 1)
 config.package.version = new_version
 
 try { fs.writeFileSync("./src-tauri/tauri.conf.json", JSON.stringify(config, null, "\t")) }
-catch (error) { exitWithError("could not write to tauri.conf.json because: " + error) }
+catch (error) { exitWithError("Could not write to tauri.conf.json because: " + error) }
 
 ////////////  Clean artifacts from target/bundle  /////////////////////////////////////
 
 try { fs.rmSync("./src-tauri/target/release/bundle", { recursive: true, force: true }) }
-catch (error) { exitWithError("could not clean releases because: " + error) }
+catch (error) { exitWithError("Could not clean releases because: " + error) }
 
 
-///////////  Publish new version on Git /////////////////////////////////////////////// 
+///////////  Publish new version on Git ///////////////////////////////////////////////
 
 const { execSync } = require("child_process");
 
 try { execSync("git add .") }
-catch(error) { exitWithError("could not 'git add .' because: " + error) }
+catch(error) { exitWithError("Could not 'git add .' because: " + error) }
 
 try { execSync(`git commit -m "release v${version}"`) }
-catch(error) { exitWithError("could not 'git add .' because: " + error) }
+catch(error) { exitWithError("Could not 'git add .' because: " + error) }
 
 try { execSync("git push") }
-catch(error) { exitWithError("could not 'git add .' because: " + error) }
+catch(error) { exitWithError("Could not 'git add .' because: " + error) }
